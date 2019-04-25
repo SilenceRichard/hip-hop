2b96846e86fd78d92fe8392f5b530718606fe687
SilenceRichard <zzxy1995@qq.com>
2019-04-25 19:46:38 +0800

    封装了footer组件， 在utils里加入了wx.showModal()的Promise方法，post与get请求的Promise方法 均可以在组件示例页面查看示例

--- client/components/footer/footer.js
+++ client/components/footer/footer.js
@@ -1,1 +1,36 @@
-Page({});
+Component({
+    properties:{
+        type:{
+            type:String,
+            value:''
+            //页脚类型,默认为空，不带链接
+            //normal 带一个底部导航链接
+            //fixed 固定页面，相当于底部图钉
+            //double 双链接
+        },
+        url:{
+            type:String,
+            value:''
+        },//底部链接,好像不允许跨域
+        leftUrl:{
+            type:String,
+            value:''
+        },//左底部链接
+        rightUrl:{
+            type:String,
+            value:''
+        },//右底部链接
+        text:{
+            type:String,
+            value:'底部链接'
+        },//单链接时的链接文字
+        leftText:{
+            type:String,
+            value:'底部链接'
+        },//双链接时左边的链接文字
+        rightText:{
+            type:String,
+            value:'底部链接'
+        },//双链接时右边的链接文字
+    }
+});
--- client/utils/util.js
+++ client/utils/util.js
@@ -13,7 +13,91 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+/**
+ *将微信小程序中的回调封装成Promise的形式，避免回调地狱
+ * **/
+//添加finally：因为还有一个参数里面还有一个complete方法。
+Promise.prototype.finally = function (callback) {
+  let P = this.constructor;
+  return this.then(
+      value => P.resolve(callback()).then(() => value),
+      reason => P.resolve(callback()).then(() => { throw reason })
+  );
+};
+//封装异步api
+const wxPromisify = fn => {
+  return function (obj = {}) {
+    return new Promise((resolve, reject) => {
+      obj.success = function (res) {
+        resolve(res)
+      }
+
+      obj.fail = function (res) {
+        reject(res)
+      }
+
+      fn(obj)
+    })
+  }
+}
+const getLocationPromisified = wxPromisify(wx.getLocation);//获取经纬度
+const showModalPromisified = wxPromisify(wx.showModal);//弹窗
+const post = (url,data) => {
+  var promise = new Promise((resolve, reject) => {
+    //网络请求
+    wx.request({
+      url: url,
+      data: data,
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'token':wx.getStorageSync('token')
+      },
+      success: function (res) {//服务器返回数据
+        if (res.statusCode == 200) {
+          resolve(res);
+        } else {//返回错误提示信息
+          reject(res.data);
+        }
+      },
+      error: function (e) {
+        reject('网络出错');
+      }
+    })
+  });
+  return promise;
+}// 封装post请求
+const get = (url, data) => {
+  var promise = new Promise((resolve, reject) => {
+    //网络请求
+    wx.request({
+      url: url,
+      data: data,
+      header: {
+        'content-type': 'application/json',
+        'token': wx.getStorageSync('token')
+      },
+      success: function (res) {//服务器返回数据
+        if (res.statusCode == 200) {
+          resolve(res);
+        } else {//返回错误提示信息
+          reject(res.data);
+        }
+      },
+      error: function (e) {
+        reject('网络出错');
+      }
+    })
+  });
+  return promise;
+}// 封装get请求
+
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatNumber: formatNumber,
+  getLocationPromisified:getLocationPromisified, //获取经纬度
+  showModalPromisified:showModalPromisified,//弹窗
+  post:post,
+  get:get
 }
