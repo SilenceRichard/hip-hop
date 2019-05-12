8017f465461b9c28f06ebc24465f29ec243762b9
maple <oncwnuKqE9XDss-SoWeXvCFuFwu0@git.weixin.qq.com>
2019-05-12 17:23:06 +0800

    约舞页面完成。

--- client/pages/dance-settings/dance-settings.js
+++ client/pages/dance-settings/dance-settings.js
@@ -25,10 +25,37 @@ Page({
       {name: 'Reggae', value: 'Reggae'},
       {name: '不限', value: '不限'},
     ],//记录舞种信息
+    contactItems:[{name:'手机号',flag:false},{name:'微信',flag:false},{name:'微信群聊',flag:false}], //联系方式
     timestamp:{
       date:util.formatDate(new Date()),
       time:"00:00"
-    }//时间戳
+    },//时间戳
+    contactName:'', //当前选择联系方式项
+  },
+  setInfo(ev){//点击事件
+    console.log(ev)
+    let obj = this.data.info;//信息中间量
+    let step0 = this.data.step;//step中间量
+    step0 ++;
+    if (this.data.step == 0){//如果是第一步
+      obj.identify = ev.target.dataset.identify; //身份
+    }
+    if (this.data.step == 1) {//如果是第二步
+      obj.type = ev.target.dataset.type; //活动形式
+    }
+    if (this.data.step == 2){//如果是第三步
+      obj.dance_type = this.data.checkboxItems;//舞种
+    }
+    if (this.data.step == 3) {//如果是第四步
+      obj.time = `${this.data.timestamp.date} ${this.data.timestamp.time}`//一系列信息
+    }
+    if (this.data.step == 4){//如果是第五步
+
+    }//上传至云端
+    this.setData({
+      info:obj,//由中间量导入数据到info
+      step:step0//更新step
+    })
   },
   bindDateChange(ev){
     console.log(ev);
@@ -45,33 +72,9 @@ Page({
         timestamp:timeobj //时间选择
       })
     }
-  }, //日期选择(这里的触发事件是bindchange)
-  setInfo(ev){//点击事件
-    console.log(ev)
-    let obj = this.data.info;//中间量
-    let step0 = this.data.step;//中间量
-    step0++;
-    if (this.data.step == 0){
-      obj.identify = ev.target.dataset.identify; //身份
-    }
-    if (this.data.step == 1) {
-      obj.type = ev.target.dataset.type; //活动形式
-    }
-    if (this.data.step ==2){
-      obj.dance_type = this.data.checkboxItems;//舞种
-    }
-    if (this.data.step == 3) {
-      obj.time = `${this.data.timestamp.date} ${this.data.timestamp.time}`
-    }
-    this.setData({
-      info:obj,
-      step:step0
-    })//由中间量导入数据
-
-    console.log(this.data.info,'----------',this.data.step)
-  },
+  },//日期选择(这里的触发事件是bindchange)
   chooseImage() {//上传图片点击事件
-      let  that = this;
+      let that = this;
       let obj = this.data.info;
       wx.chooseImage({
         count:1,
@@ -82,6 +85,18 @@ Page({
           })
         }
       })
+  },
+  chooseImageQR(){
+    let obj = this.data.info;
+    let that = this;
+    wx.chooseImage({count:1,
+      success:function (res) {
+      obj.QR_code = res.tempFilePaths[0]
+        that.setData({
+          info:obj
+        })
+       }
+    })
   },
   chooseLocation() {
     let that = this;
@@ -134,7 +149,7 @@ Page({
   },//加人数
   minusNumber(){
     let obj = this.data.info;
-    if (obj.limit-1 == '0'){
+    if (obj.limit -1 == '0'){
       obj.limit = '无限制'
     }
     else {
@@ -143,7 +158,7 @@ Page({
     this.setData({
       info:obj
     })
-  }, //减人数
+  },//减人数
   changeTitle(e){
     let obj = this.data.info;
     obj.title = e.detail.value;
@@ -157,7 +172,7 @@ Page({
     this.setData({
       info:obj
     })
-  },//
+  },
   changeCondition(e){
     let obj = this.data.info;
     obj.condition = e.detail.value;
@@ -165,59 +180,31 @@ Page({
       info:obj
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  changeContact(e){
+    let obj = this.data.contactItems;
+    obj.forEach(item =>{
+      item.flag = false
+    })
+    let infoObj= this.data.info;
+    infoObj.contact = e.target.dataset.name
+    obj.forEach(
+        item =>{
+           if (item.name == e.target.dataset.name){
+             item.flag = true;
+             this.setData({
+                contactName:item.name,
+                contactItems:obj,
+                info:infoObj
+             })
+           }
+        }
+    )
+  },//改变选择的联系方式
+  changePhoneNumber(e){
+     let obj = this.data.info;//设置中间值
+     obj.phone =  e.detail.value;
+     this.setData({
+       info:obj
+     })
+    },
 })
