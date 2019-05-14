8e5a72e4aacbdb780d5efff53a149d591dc335bb
scarecrew <1589982958@qq.com>
2019-05-14 23:12:02 +0800

    资讯详情

--- client/pages/applyInfo/applyInfo.js
+++ client/pages/applyInfo/applyInfo.js
@@ -3,8 +3,26 @@ Page({
   toMineInfo() {
     wx.navigateTo({ url: '../mine-person/mine-person' })
   },
- 
-  onShareAppMessage: function () {
+  data: {
+    info:{}
+  },
 
+  sendApply(){
+    wx.cloud.callFunction({
+      name:"home",
+      data:{
+        method:"sendInfo",
+        info:this.data.info,
+        openid: app.data.openid
+      },
+      success(res){
+        console.log(res)
+      }
+    })
+  },
+  changeInfo(){
+    this.setData({
+      info: apply
+    })
   }
 })
--- client/pages/mine-systemInfo/mine-systemInfo.js
+++ client/pages/mine-systemInfo/mine-systemInfo.js
@@ -6,32 +6,26 @@ Page({
     },
     data: {
       info: [{
-        imgsrc: '../../static/home.png',
+        imgsrc: '../../static/icon/ironman.png',
         title: '标题',
-        innerText1: 'popping',
-        innerText2: 'cypher',
-        limit_now: '4',
-        limit: '10',
+        innerText1: '你已经加入了钢铁侠的约局',
+        innerText2: '钢铁侠同意了你的报名申请',
         time: '2019-5-11 00:00:00',
         location: '昌平'
       },
         {
-          imgsrc: '../../static/home.png',
+          imgsrc: '../../static/icon/cap.png',
           title: '标题',
-          innerText1: 'popping',
-          innerText2: 'cypher',
-          limit_now: '4',
-          limit: '10',
+          innerText1: '美国队长想要加入你的约局',
+          innerText2: '哇！美国队长对你的约局感兴趣',
           time: '2019-5-11 00:00:00',
           location: '昌平'
         },
         {
-          imgsrc: '../../static/home.png',
+          imgsrc: '../../static/icon/blackpathen.png',
           title: '标题',
-          innerText1: 'popping',
-          innerText2: 'cypher',
-          limit_now: '4',
-          limit: '10',
+          innerText1: '复仇者联盟已经组队成功',
+          innerText2: '恭喜！您的约局复仇者联盟已经组队成功',
           time: '2019-5-11 00:00:00',
           location: '昌平'
         },
