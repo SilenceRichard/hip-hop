0f2e44d4fead68877c29c66e1909968deb0316fa
scarecrew <1589982958@qq.com>
2019-05-18 21:43:40 +0800

    资讯详情

--- client/pages/home-appointInfo/home-appointInfo.js
+++ client/pages/home-appointInfo/home-appointInfo.js
@@ -13,12 +13,21 @@ Page({
             limit: '10',
             activeInfo: '我要好好跳舞',
             joinInfo:'你能好好跳舞吗',
-            activeTime:'2019 5 12',
+            activeDate:'2019 5 12',
+            activeTime: '12:00',
             activeLocation:'北京邮电高中',
             head:'../../static/icon/strange.png',
             name:'我叫王大可',
-            id:''
-        }
+            id:'',
+            nickName: '王大可',
+            indentity: '沙雕',
+            sex: '男的一匹',
+            location: '北京邮电幼儿园',
+            danceType: 'jazz',
+            danceAge: '6年',
+            sigh: '肉松小贝里有肉松'
+        },
+        showModalFlag:false
     },
 
     onLoad(info){
@@ -28,18 +37,44 @@ Page({
         })
       //  已解决 console.log("data.info------",this.data.info)
     },
+    toApplyInfo() {
+    let obj = this.data.info.id;
+    wx.navigateTo({ url: '../home-applyInfo/home-applyInfo?id=' + obj })
+  },
 
-    toApplyInfo(){
-      let obj = this.data.info.id;
-      wx.navigateTo({ url: '../home-applyInfo/home-applyInfo?id='+obj})
-    },
+  
+  showModal(e) {
+    console.log(11111)
+    this.setData({
+      showModalFlag:true
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      showModalFlag:false
+    })
+  },
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
+  },
 
+   
     onReady: function () {
         var that = this;
         wx.cloud.callFunction({
             name: 'home',
             data: {
-                methed: "getAppointInfo",
+                method: "getAppointInfo",
                 info:id
             },
             success: function (res) {
