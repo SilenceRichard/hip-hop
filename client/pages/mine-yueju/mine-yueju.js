13dd00898ed0f70ce16e4942c02b0e21af4ed6cb
SilenceRichard <zzxy1995@qq.com>
2019-05-20 21:51:12 +0800

    修改了个小问题

--- client/pages/mine-yueju/mine-yueju.js
+++ client/pages/mine-yueju/mine-yueju.js
@@ -3,7 +3,25 @@ var app = getApp();
 Page({
     data:{
         myJoined:[],
-        mySetted:[]
+        mySetted:[],
+        flag:'fq',  //导航标志，我发起的/我加入的
+        tabList:[{name:'我发起的',type:'fq'},{name:'我加入的',type:'jr'}],
+        TabCur:0
+    },
+    goTo(ev){
+        console.log(ev)
+        if (ev.currentTarget.dataset.type=="fq"){
+            this.setData({
+                TabCur:ev.currentTarget.dataset.id,
+                flag:'fq'
+            })
+        }
+        else if(ev.currentTarget.dataset.type=="jr"){
+            this.setData({
+                TabCur:ev.currentTarget.dataset.id,
+                flag:'jr'
+            })
+        }
     },
     onReady:async function () {
         console.log("请求参数:",{
@@ -17,18 +35,30 @@ Page({
               openid:app.data.openid
           }
       })
-      res.result.joinedDance.map(item =>{
-           let a =[]; //处理舞种
-          item.dance_type.map(val=>{
-             if (val.checked){
-                 a.push(val.name)
-             }
-          })
-          item.dance_type_show = a;
-          return item
-      })
+      console.log("RES:",res)
+        res.result.Dance.map(item =>{
+            let a =[]; //处理舞种
+            item.dance_type.map(val=>{
+                if (val.checked){
+                    a.push(val.name)
+                }
+            })
+            item.dance_type_show = a;
+            return item
+        })
+        res.result.sentDance.map(item =>{
+            let a =[]; //处理舞种
+            item.dance_type.map(val=>{
+                if (val.checked){
+                    a.push(val.name)
+                }
+            })
+            item.dance_type_show = a;
+            return item
+        })
       this.setData({
-          myJoined:res.result.joinedDance
+          myJoined:res.result.Dance,
+          mySetted:res.result.sentDance
       })
     }
 })
