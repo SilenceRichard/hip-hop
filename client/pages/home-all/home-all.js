ba83e121492919c7f1467715d4de6459027f9e68
Ducky-Suki <919383107@qq.com>
2019-05-23 20:12:37 +0800

    说明

--- client/pages/home-all/home-all.js
+++ client/pages/home-all/home-all.js
@@ -19,163 +19,170 @@ Page({
       location:'',
       str: ''
     },
-    {
-      cover: '../../static/home.png',
-      title: '标题',
-      dance_type: 'popping',
-      type: 'cypher',
-      limit: '4/10',
-      time: '2019-5-11 00:00:00',
-      location: '昌平',
-      str: ''
-    },
-    {
-      cover: '../../static/home.png',
-      title: '标题',
-      dance_type: 'popping',
-      type: 'cypher',
-      limit: '4/10',
-      time: '2019-5-11 00:00:00',
-      location: '昌平',
-      str: ''
-    },
+      {
+        cover: '../../static/home.png',
+        title: '标题',
+        dance_type: 'popping',
+        type: 'cypher',
+        limit: '4/10',
+        time: '2019-5-11 00:00:00',
+        location: '昌平',
+        str: ''
+      },
+      {
+        cover: '../../static/home.png',
+        title: '标题',
+        dance_type: 'popping',
+        type: 'cypher',
+        limit: '4/10',
+        time: '2019-5-11 00:00:00',
+        location: '昌平',
+        str: ''
+      },
     ],
     hotList:['111','222','333','111111','111111'],
     historyList:['111','222','333','111111','111111'],
-    keyward:'',
+    keyword:'',
     dropDownFlag: false,
-    searchFlag:false
-  },
-  Back(){
-    this.setData({
-      searchFlag : false
-    })
+    searchFlag:false,
+    searchKeyFlag:false
   },
 
-  clickTag(ev){
-    console.log("进入clickTag",ev)
-    this.setData({
-      keyword: ev.target.dataset.item
-    })
-    console.log("此时搜索的keyword------",this.data.keyword)
-  },
 
-  inputKeyword(ev){
-    console.log("进入inputKeyword",ev)
-    this.setData({
-      keyword : ev.detail.value
-    })
-    console.log(this.data.keyword)
-  },
 
-  search() {
-    let that = this;
-    console.log("进入search,传入的是", this.data.keyword);
-    // this.setData({
-    //   searchFlag: false
-    // })
-    wx.cloud.callFunction({
-      name:"home",
-      data:{
-        method:"getInfo",
-        type:"keyword",
-        info:this.data.keyword,
-        openid:app.data.openid
-      },
-      success(res){
-        //console.log("传入的是", this.data.keyword)
-        console.log("返回搜索",res)
-        that.setData({
-          info: res.result.checkResult
-        })
-      },
-      fail(err){
-        console.log(`getInfo错误：${err}`)
-      }
-    })
-  },
+clickTag(ev){
+  console.log("进入clickTag",ev)
+  this.setData({
+    keyword: ev.target.dataset.item
+  })
+  console.log("此时搜索的keyword------",this.data.keyword)
+},
 
-  activeSearch:async function(){
-    this.setData({
-      searchFlag : true
-    })
-    let res1 = await wx.cloud.callFunction({ name: "home", data: { method:"getInfo", type:"hot",openid:app.data.openid}});
-    console.log("返回热门搜索-------------", res1);
-    this.setData({hotList : res1.result.checkResult});
-    console.log("hotList :", this.data.hotList);
-    console.log("发送的参数：",app.data.openid)
-    let res2 = await wx.cloud.callFunction({ name: "home", data: { method:"getInfo", type:"history",openid:app.data.openid}});
-    console.log("返回历史搜索-------------", res2);
-    this.setData({historyList : res2.result.checkResult});
-  },
+inputKeyword(ev){
+  console.log("进入inputKeyword",ev)
+  this.setData({
+    keyword : ev.detail.value
+  })
+  console.log(this.data.keyword)
+},
 
-  activeDropDown1() {
-    var that = this;
-    // console.log("程序进来啦-----")
-    that.setData({
-      dropDownFlag: false
-    }),
+search() {
+  let that = this;
+  this.setData({
+    searchFlag:false,
+    searchKeyFlag:true
+  })
+  console.log("进入search,传入的是", this.data.keyword);
+  wx.cloud.callFunction({
+    name:"home",
+    data:{
+      method:"getInfo",
+      type:"keyword",
+      info:this.data.keyword,
+      openid:app.data.openid
+    },
+
+    success(res){
+      //console.log("传入的是", this.data.keyword)
+      console.log("返回搜索",res)
+      that.setData({
+        info: res.result.checkResult
+      })
+    },
+    fail(err){
+      console.log(`getInfo错误：${err}`)
+    }
+  })
+},
+
+cancel(){
+  this.setData({
+    searchFlag:false,
+    searchKeyFlag:false
+  })
+},
+
+activeSearch:async function(){
+  this.setData({
+    searchFlag : true
+  })
+  let res1 = await wx.cloud.callFunction({ name: "home", data: { method:"getInfo", type:"hot",openid:app.data.openid}});
+  console.log("返回热门搜索-------------", res1);
+  this.setData({hotList : res1.result.checkResult});
+  // console.log("hotList :", this.data.hotList);
+  // console.log("发送的参数：",app.data.openid)
+  let res2 = await wx.cloud.callFunction({ name: "home", data: { method:"getInfo", type:"history",openid:app.data.openid}});
+  // console.log("返回历史搜索-------------", res2);
+  this.setData({historyList : res2.result.checkResult});
+},
+
+activeDropDown1() {
+  var that = this;
+  // console.log("程序进来啦-----")
+  that.setData({
+    dropDownFlag: false
+  }),
       wx.cloud.callFunction(
-        {
-          name: "home",
-          data: {
-            method: "getInfo",
-            type: "time"
-          },
-          success: function (res) {
-            //后台规定参数返回形式
-            //return{
-            //checkResult: [{},{},{}]
-            // }
-            res.result.checkResult.forEach(item => {
-              item.str = ''
-              item.dance_type.forEach(val => {
-                // var a=0;
-                // if (val.checked == true && a == 0){
-                //     item.str= val.name;
-                //     a++;
-                // }
-                // else if(val.checked == true){
-                //     item.str=item.str+ ','+ val.name;
-                //     a++;
-                // }
-                if (val.checked == true) {
-                  item.str = item.str + val.name + ',';
-                }
+          {
+            name: "home",
+            data: {
+              method: "getInfo",
+              type: "time"
+            },
+            success: function (res) {
+              //后台规定参数返回形式
+              //return{
+              //checkResult: [{},{},{}]
+              // }
+              res.result.checkResult.forEach(item => {
+                item.str = ''
+                item.dance_type.forEach(val => {
+                  // var a=0;
+                  // if (val.checked == true && a == 0){
+                  //     item.str= val.name;
+                  //     a++;
+                  // }
+                  // else if(val.checked == true){
+                  //     item.str=item.str+ ','+ val.name;
+                  //     a++;
+                  // }
+                  if (val.checked == true) {
+                    item.str = item.str + val.name + ',';
+                  }
+                })
               })
-            })
-            res.result.checkResult.forEach(item => {
-              item.str = item.str.slice(0, -1)
-            }),
+              res.result.checkResult.forEach(item => {
+                item.str = item.str.slice(0, -1)
+              }),
 
-              // this,that,傻傻分不清楚
-              that.setData({
-                info: res.result.checkResult
-              })
+                  // this,that,傻傻分不清楚
+                  that.setData({
+                    info: res.result.checkResult
+                  })
 
-            // let obj = that.data.info;
-            // obj.imgsrc = res.imgsrc;
-            // obj.title  = res.title ;
-            // obj.innerText1 = res.innerText1;
-            // obj.innerText2 = res.innerText2;
-            // obj.limit_now  = res.limit_now;
-            // obj.limit      = res.limit;
-            // obj.time      = res.time;
-            // obj.location      = res.location;
+              // let obj = that.data.info;
+              // obj.imgsrc = res.imgsrc;
+              // obj.title  = res.title ;
+              // obj.innerText1 = res.innerText1;
+              // obj.innerText2 = res.innerText2;
+              // obj.limit_now  = res.limit_now;
+              // obj.limit      = res.limit;
+              // obj.time      = res.time;
+              // obj.location      = res.location;
+            }
           }
-        }
       )
-  },
-  activeDropDown2() {
-    console.log("程序进来了，按距离排序");
-    var that = this;
-    that.setData({
-      dropDownFlag: false
-    })
-    wx.getLocation({
-      success(res) {
-        console.log("获取用户纬度经度：", res.latitude, res.longitude);
-        wx.cloud.callFunction(
+},
+activeDropDown2() {
+  console.log("程序进来了，按距离排序");
+  var that = this;
+  that.setData({
+    dropDownFlag: false
+  })
+  wx.getLocation({
+    success(res) {
+      console.log("获取用户纬度经度：", res.latitude, res.longitude);
+      wx.cloud.callFunction(
           {
             name: "home",
             data: {
@@ -197,127 +204,127 @@ Page({
                 item.str = item.str.slice(0, -1)
               }),
 
-                that.setData({
-                info: res_.result.checkResult
-                })
+                  that.setData({
+                    info: res_.result.checkResult
+                  })
               console.log(res_)
             }
           }
-        )
-      }
-    })
-  },
+      )
+    }
+  })
+},
 
-  activeList(ev) {
-    // console.log(ev.currentTarget.dataset.item)
-    // ev.currentTarget.dataset.item.flag = true;
-    let arr = this.data.list;
-    var that = this;
-    for (var j = 0; j < arr.length; j++) {
-      arr[j].flag = false;
-    }
-    this.setData({
-      list: arr
-    })
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].name == ev.currentTarget.dataset.item.name) {
-        arr[i].flag = true;
-        that.setData({
-          list: arr
-        }
+activeList(ev) {
+  // console.log(ev.currentTarget.dataset.item)
+  // ev.currentTarget.dataset.item.flag = true;
+  let arr = this.data.list;
+  var that = this;
+  for (var j = 0; j < arr.length; j++) {
+    arr[j].flag = false;
+  }
+  this.setData({
+    list: arr
+  })
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].name == ev.currentTarget.dataset.item.name) {
+      arr[i].flag = true;
+      that.setData({
+            list: arr
+          }
 
-        )
-      }
+      )
     }
-    if (ev.currentTarget.dataset.item.name == "综合") {
-      that.setData({
-        dropDownFlag: true
-      })
-    }
-    else if (ev.currentTarget.dataset.item.name == "个人") {
-      that.setData({
-        dropDownFlag: false
-      });
-      wx.cloud.callFunction({
-        name: "home",
-        data: {
-          method: 'getInfo',
-          type: 'getByIndividual',
-          location: app.data.location
-        },
-        success(res) {
-          // console.log("按个人查询返回res",res);
-          res.result.checkResult.forEach(item => {
-            item.str = ''
-            item.dance_type.forEach(val => {
-              if (val.checked == true) {
-                item.str = item.str + val.name + ',';
-              }
-            })
+  }
+  if (ev.currentTarget.dataset.item.name == "综合") {
+    that.setData({
+      dropDownFlag: true
+    })
+  }
+  else if (ev.currentTarget.dataset.item.name == "个人") {
+    that.setData({
+      dropDownFlag: false
+    });
+    wx.cloud.callFunction({
+      name: "home",
+      data: {
+        method: 'getInfo',
+        type: 'getByIndividual',
+        location: app.data.location
+      },
+      success(res) {
+        // console.log("按个人查询返回res",res);
+        res.result.checkResult.forEach(item => {
+          item.str = ''
+          item.dance_type.forEach(val => {
+            if (val.checked == true) {
+              item.str = item.str + val.name + ',';
+            }
           })
-          res.result.checkResult.forEach(item => {
-            item.str = item.str.slice(0, -1)
-          }),
+        })
+        res.result.checkResult.forEach(item => {
+          item.str = item.str.slice(0, -1)
+        }),
 
             that.setData({
               info: res.result.checkResult
             })
-          console.log(res)
-        }
-      })
-    }
-    else {
-      that.setData({
-        dropDownFlag: false
-      });
-      wx.cloud.callFunction({
-        name: "home",
-        data: {
-          method: 'getInfo',
-          type: 'getByOfficial'
-        },
-        success(res) {
-          // console.log("按官方查询返回res", res);
-          res.result.checkResult.forEach(item => {
-            item.str = ''
-            item.dance_type.forEach(val => {
-              if (val.checked == true) {
-                item.str = item.str + val.name + ',';
-              }
-            })
+        console.log(res)
+      }
+    })
+  }
+  else {
+    that.setData({
+      dropDownFlag: false
+    });
+    wx.cloud.callFunction({
+      name: "home",
+      data: {
+        method: 'getInfo',
+        type: 'getByOfficial'
+      },
+      success(res) {
+        // console.log("按官方查询返回res", res);
+        res.result.checkResult.forEach(item => {
+          item.str = ''
+          item.dance_type.forEach(val => {
+            if (val.checked == true) {
+              item.str = item.str + val.name + ',';
+            }
           })
-          res.result.checkResult.forEach(item => {
-            item.str = item.str.slice(0, -1)
-          }),
+        })
+        res.result.checkResult.forEach(item => {
+          item.str = item.str.slice(0, -1)
+        }),
 
             that.setData({
               info: res.result.checkResult
             })
-          console.log(res)
-        }
-      })
-    }
-  },
+        console.log(res)
+      }
+    })
+  }
+},
 
-  goToDetail(ev){
-    // console.log("这是ev---------",ev)
-    wx.navigateTo({url:"../home-appointInfo/home-appointInfo?id="+ev.currentTarget.dataset.item._id})
-  },
+goToDetail(ev){
+  console.log("这是ev---------",ev)
+  wx.navigateTo({url:"../home-appointInfo/home-appointInfo?id="+ev.currentTarget.dataset.item._id})
+},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+/**
+ * 生命周期函数--监听页面加载
+ */
+onLoad: function (options) {
 
-  },
+},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    console.log("页面进来啦-------")
-    var that = this;
-    wx.cloud.callFunction(
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady: function () {
+  // console.log("页面进来啦-------") 没问题
+  var that = this;
+  wx.cloud.callFunction(
       {
         name: 'home',
         data: {
@@ -341,11 +348,11 @@ Page({
             item.str = item.str.slice(0, -1)
           }),
 
-            // this,that,傻傻分不清楚
-            that.setData({
-              info: res.result.checkResult
-            })
-          console.log(res)
+              // this,that,傻傻分不清楚
+              that.setData({
+                info: res.result.checkResult
+              })
+          console.log("这是返回的全部约局资讯------",res)
           // let obj = that.data.info;
           // obj.imgsrc = res.imgsrc;
           // obj.title  = res.title ;
@@ -358,50 +365,48 @@ Page({
         }
 
       })
-  },
+},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function () {
 
-  },
+},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function () {
 
-  },
+},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function () {
 
-  },
+},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function () {
 
-  },
+},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function () {
 
-  },
+},
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function () {
 
-  }
+}
 })
-
-
 
--- client/pages/home-appointInfo/home-appointInfo.js
+++ client/pages/home-appointInfo/home-appointInfo.js
@@ -1,3 +1,6 @@
+import util from '../../utils/util'
+import regeneratorRuntime from '../../utils/wxPromise.min.js' //引入async await语法糖
+const app = getApp();
 
 Page({
     data: {
@@ -7,7 +10,7 @@ Page({
             identify: '个人',
             dance_type:"popping",
             type:"cypher",
-            time: '2019 5 12',
+            date: '2019 5 12',
             limit: '4',
             limit_set: '10',
             content: '我要好好跳舞',
@@ -17,14 +20,15 @@ Page({
             location:'北京邮电高中',
             avatarUrl:'../../static/icon/strange.png',
             name:'我叫王大可',
-            id:'',
+            _id:'',
             nickName: '王大可',
             role: '沙雕',
             sex: '男的一匹',
             city: '北京邮电幼儿园',
             danceType: 'jazz',
             danceAge: '6年',
-            introduction: '肉松小贝里有肉松'
+            introduction: '肉松小贝里有肉松',
+            _openid:''
         },
         info_:{},
         //info_,
@@ -32,107 +36,113 @@ Page({
     },
 
     onLoad(info){
-        console.log("参数传进来了------",info)
+        // console.log("参数传进来了------",info)已解决
         this.setData({
-          "info.id":info.id
+            "info._id":info.id
         })
-      //  已解决 console.log("data.info------",this.data.info)
+        console.log("data.info._id------",this.data.info._id)
     },
     toApplyInfo() {
-    let obj = this.data.info.id;
-    wx.navigateTo({ url: '../home-applyInfo/home-applyInfo?id=' + obj })
-  },
+        var that = this;//闭包了
+        let obj = that.data.info._id;
+        wx.navigateTo({ url: '../home-applyInfo/home-applyInfo?id=' + obj })
 
-  
-  showModal(e) {
-    console.log(11111)
-    this.setData({
-      showModalFlag:true
-    })
-    wx.cloud.callFunction({
-      name: 'home',
-      data: {
-        method: "getFounderInfo",
-        info: id
-      },
-      success: function (res) {
-        console.log("传回来的是--------", res)
-        that.setData({
-          info: res.result.checkResult
+        console.log("传到报名页面的obj是------",obj)
+    },
+
+
+    showModal(e) {
+        console.log("showModal的返回值-----",e)
+        this.setData({
+            showModalFlag:true
         })
-      }
-    })
-  },
-  hideModal(e) {
-    this.setData({
-      showModalFlag:false
-      })
-  },
-  ChooseCheckbox(e) {
-    let items = this.data.checkbox;
-    let values = e.currentTarget.dataset.value;
-    for (let i = 0, lenI = items.length; i < lenI; ++i) {
-      if (items[i].value == values) {
-        items[i].checked = !items[i].checked;
-        break
-      }
-    }
-    this.setData({
-      checkbox: items
-    })
-  },
-
-   
-    onReady: function () {
-        var that = this;
         wx.cloud.callFunction({
             name: 'home',
             data: {
-                method: "getAppointInfo",
-                info: this.data.info.id
+                method: "getFounderInfo",
+                info: id
             },
             success: function (res) {
-              console.log("第一次调用成功res:", res)
-                //处理舞种信息
-                var dance_type_ =[];
-                res.result.checkResult[0].dance_type.forEach(function (element) {
-                  if (element.checked == true){
-                    dance_type_.push(element.name);
-                  }
-                });
-                res.result.checkResult[0].dance_type = dance_type_;
-                console.log("处理舞种信息得到--------", res)
-
+                console.log("传回来的是--------", res)
                 that.setData({
-                    info: res.result.checkResult[0]
-                })
-
-                //console.log("下面查询openid：", res.result.checkResult[0].openid);
-                //再次调用云函数
-                wx.cloud.callFunction({
-                  name: 'mine',
-                  data: {
-                    method: "getUserInfo",
-                    openid: res.result.checkResult[0].openid
-                  },
-                  success: function (e) {
-                    //console.log("第二次调用成功e:",e)
-
-                    //处理舞种信息
-                    var dance_type_ = [];
-                    e.result.res.danceType.forEach(function (element) {
-                      if (element.checked == true) {
-                        dance_type_.push(element.name);
-                      }
-                    });
-                    e.result.res.danceType = dance_type_;
-
-                    that.setData({
-                      info_: e.result.res
-                    })
-                  }
+                    info: res.result.checkResult
                 })
             }
         })
+    },
+    hideModal(e) {
+        this.setData({
+            showModalFlag:false
+        })
+    },
+    ChooseCheckbox(e) {
+        let items = this.data.checkbox;
+        let values = e.currentTarget.dataset.value;
+        for (let i = 0, lenI = items.length; i < lenI; ++i) {
+            if (items[i].value == values) {
+                items[i].checked = !items[i].checked;
+                break
+            }
+        }
+        this.setData({
+            checkbox: items
+        })
+    },
+
+
+    onReady: async function () {
+        let that = this;
+
+         wx.cloud.callFunction({
+            name: 'home',
+            data: {
+               method: "getAppointInfo",
+               info: this.data.info._id
+            },
+          success: function (res) {
+              console.log("第一次调用成功res:", res)
+           console.log("下面查询openid：", res.result.checkResult[0]._openid)//发现_openid和openid查询结果一样
+              //处理舞种信息
+              var dance_type_ =[];
+                 res.result.checkResult[0].dance_type.forEach(function (element) {
+                  if (element.checked == true){
+                     dance_type_.push(element.name);
+                   }
+                 });
+                 res.result.checkResult[0].dance_type = dance_type_;
+                console.log("处理舞种信息得到--------", res) //已解决
+
+               that.setData({
+                     info: res.result.checkResult[0]
+                 })
+                 console.log("本地的openid",that.data.info._openid)
+
+        console.log("下面查询openid：", res.result.checkResult[0].openid)
+        //再次调用云函数
+         wx.cloud.callFunction({
+           name: 'mine',
+           data: {
+             method: "getUserInfo",
+             openid: res.result.checkResult[0].openid  //openid =>  _openid
+           },
+           success: function (e) {
+            console.log("第二次调用成功e:",e)      //result为null
+
+        //处理舞种信息
+               var dance_type_ = [];
+               e.result.res.danceType.forEach(function (element) {
+                   if (element.checked == true) {
+                       dance_type_.push(element.name);
+                   }
+               });
+               e.result.res.danceType = dance_type_;
+
+        that.setData({
+           info_: e.result.res
+              })
+          }
+          })
+            }
+           })
     }
 })
